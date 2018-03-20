048a259632591245986a570a002fb87ecd25a6cd
Tomáš Kudělka <thorewi@gmail.com>
2018-03-20 09:16:11 +0100

    use \Nette\SmartObject

--- src/Kdyby/DoctrineForms/EntityFormMapper.php
+++ src/Kdyby/DoctrineForms/EntityFormMapper.php
@@ -23,8 +23,10 @@ use Symfony\Component\PropertyAccess\PropertyAccessor;
 /**
  * @author Filip Procházka <filip@prochazka.su>
  */
-class EntityFormMapper extends Nette\Object
+class EntityFormMapper
 {
+	
+	use \Nette\SmartObject;
 
 	/**
 	 * @var EntityManager

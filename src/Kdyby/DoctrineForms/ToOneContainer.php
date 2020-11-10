a42916691776abbbae919718f8fbb4badb4abfad
Tomáš Kudělka <thorewi@gmail.com>
2020-11-10 11:24:49 +0000

    Update ToOneContainer.php

--- src/Kdyby/DoctrineForms/ToOneContainer.php
+++ src/Kdyby/DoctrineForms/ToOneContainer.php
@@ -7,9 +7,18 @@ use Nette;
 
 class ToOneContainer extends Nette\Forms\Container
 {
+	protected $entityFactory;
 	protected $isFilledComponent;
 
-	protected $hasGeneratedIsFilledComponent = false;
+	public function __construct($entityFactory)
+	{
+		$this->entityFactory = $entityFactory;
+	}
+	
+	public function getEntityFactory()
+	{
+		return $this->entityFactory;
+	}
 
 	public function setIsFilledComponent(Nette\Forms\Controls\BaseControl $isFilledComponent)
 	{
@@ -22,67 +31,57 @@ class ToOneContainer extends Nette\Forms\Container
 		return $this->isFilledComponent;
 	}
 
-	public function setHasGeneratedIsFilledComponent()
+	public function isEmpty()
 	{
-		$this->hasGeneratedIsFilledComponent = true;
-		return $this;
+		return !array_filter($this->getValues('array'));
 	}
-	
-	public function hasGeneratedIsFilledComponent()
+
+	public function createEntity(\Doctrine\ORM\Mapping\ClassMetadata $relationMeta)
 	{
-		return (bool) $this->hasGeneratedIsFilledComponent;
+		if (! $this->entityFactory) {
+			return $relationMeta->newInstance();
+		}
+
+		$entityClass = $relationMeta->getName();
+		return $this->entityFactory->call($this, new $entityClass);
 	}
 
 	public static function register($name = 'toOne')
 	{
-		Nette\Forms\Container::extensionMethod($name, function (Nette\Forms\Container $_this, $name, $containerFactory, $isFilledComponentName, $errorMessage = null) {
-			$container = new ToOneContainer;
+		Nette\Forms\Container::extensionMethod($name, function (Nette\Forms\Container $_this, $name, $containerFactory, $entityFactory = null, $isFilledComponentName = null, $errorMessage = null) {
+			$container = new ToOneContainer($entityFactory);
 
 			$containerFactory->call($_this, $container);
 
-			/** @var EntityManager $em */
-			$em = $_this->getParent()->em;
+			if ($isFilledComponentName) {
+				if (isset($container[$isFilledComponentName])) {
+					throw new \Exception('Component ' . $isFilledComponentName . ' already exists.');
+				}
 
-			$mapping = $em->getMetadataFactory()
-				->getMetadataFor(get_class($_this->getParent()->getRow()))
-				->getAssociationMapping($name)['joinColumns'][0];
-			
-			if (!isset($container[$isFilledComponentName])) {
-				$container->addText($isFilledComponentName)
+				$isFilledComponent = $container->addText($isFilledComponentName)
 					->setHtmlAttribute('style', 'display: none');
 
-				// the fiels is not nullable
-				if (isset($mapping['nullable']) && $mapping['nullable'] === false) {
-					if (empty($errorMessage)) {
-						throw new \Exception('The error message must be set if isFilled component is not an existing component and the field is not nullable.');
-					}
-
-					$container[$isFilledComponentName]->setRequired($errorMessage);
+				if ($errorMessage) {
+					$isFilledComponent->setRequired($errorMessage);
 				}
 				else {
-					if ($errorMessage) {
-						throw new \Exception('The error message must not be set if the field is nullable.');
+					/** @var EntityManager $em */
+					$em = $_this->getForm()->getEntityMapper()->getEntityManager();
+
+					$mapping = $em->getMetadataFactory()
+						->getMetadataFor(get_class($_this->getParent()->getRow()))
+						->getAssociationMapping($name)['joinColumns'][0];
+
+					// the field is not nullable
+					if (isset($mapping['nullable']) && $mapping['nullable'] === false) {
+						if (empty($errorMessage)) {
+							throw new \Exception('The error message must be set if isFilled component is set and the field is not nullable.');
+						}
 					}
 				}
-				
-				$container->setHasGeneratedIsFilledComponent(true);
-			}
-			else {
-				if ($errorMessage) {
-					throw new \Exception('The error message must not be set if isFilled component is an existing component.');
-				}
 
-				// throw exception in case the field is not nullable and the [isFilled] component is not required
-				if (
-					isset($mapping['nullable']) && $mapping['nullable'] === false
-					&&
-					!$container[$isFilledComponentName]->isRequired()
-				) {
-					throw new \Exception('ToOneContainer::isFilled component has to be set as required if the field is not nullable.');
-				}				
+				$container->setIsFilledComponent($isFilledComponent);
 			}
-
-			$container->setIsFilledComponent($container[$isFilledComponentName]);
 
 			return $_this[$name] = $container;
 		});

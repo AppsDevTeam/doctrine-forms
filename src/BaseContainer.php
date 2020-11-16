5e7bd737b0e625cea85e412027f4fa4b7726d708
Tomáš Kudělka <thorewi@gmail.com>
2020-11-16 16:06:03 +0000

    Update BaseContainer.php

--- src/BaseContainer.php
+++ src/BaseContainer.php
@@ -7,26 +7,45 @@ use Nette;
 
 abstract class BaseContainer extends Nette\Forms\Container
 {
+	// because there is no "addError" method in Container class
+	// we have to create a IControl instance and call "addError" on it
+	// the control must not be an instance of "HiddenField"
+	// otherwise the error will be added to the form instead of the container
+	const ERROR_CONTROL_NAME = '_containerError_';
+
+	/**
+	 * @var string
+	 */
+	private ?string $requiredMessage = null;
+
+	public function setRequired(string $message)
+	{
+		$this->requiredMessage = $message;
+		return $this;
+	}
+
+	protected function getRequiredMessage()
+	{
+		return $this->requiredMessage;
+	}
+
+	protected function isRequired()
+	{
+		return (bool) $this->getRequiredMessage();
+	}
+
 	public static function register()
 	{
 		Nette\Forms\Container::extensionMethod('toOne', function (Nette\Forms\Container $_this, string $name, Closure $containerFactory, ?Closure $entityFactory, ?string $isFilledComponentName, ?string $isRequiredMessage) {
-			$container = (new ToOneContainerFactory($name, $containerFactory, $entityFactory, $isFilledComponentName))->create();
-
-			if ($isRequiredMessage) {
-				$_this->onValidate[] = function () use ($container, $isRequiredMessage) {
-					$container->addError($isRequiredMessage);
-				};
-			}
-
-			return $_this[$name] = $container;
+			return $_this[$name] = (new ToOneContainerFactory($name, $containerFactory, $entityFactory, $isFilledComponentName, $isRequiredMessage))
+				->create()
+				->setRequired($isRequiredMessage);
 		});
 
 		Nette\Forms\Container::extensionMethod('toMany', function (Nette\Forms\Container $_this, string $name, Closure $containerFactory, ?Closure $entityFactory, ?string $isFilledComponentName, ?string $isRequiredMessage) {
-			$container = (new ToManyContainer)
+			return $_this[$name] = (new ToManyContainer)
 				->setToOneContainerFactory(new ToOneContainerFactory($name, $containerFactory, $entityFactory, $isFilledComponentName))
 				->setRequired($isRequiredMessage);
-
-			return $_this[$name] = $container;
 		});
 	}
 }

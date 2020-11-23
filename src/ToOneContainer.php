bcada57c1edb964c52369ccfd89a54f7399ddb42
Tomáš Kudělka <thorewi@gmail.com>
2020-11-23 22:08:20 +0000

    Update ToOneContainer.php

--- src/ToOneContainer.php
+++ src/ToOneContainer.php
@@ -3,106 +3,34 @@
 namespace ADT\DoctrineForms;
 
 use Closure;
-use Doctrine\ORM\Mapping\ClassMetadata;
-use Exception;
-use Nette;
-use Nette\Application\UI\Presenter;
 
-class ToOneContainer extends BaseContainer
+class ToOneContainerFactory
 {
-	/**
-	 * @var Closure|null
-	 */
+	private string $name;
+	private ?Closure $containerFactory;
 	private ?Closure $entityFactory;
+	private ?string $isFilledComponentName;
 
 	/**
-	 * @var Nette\Forms\Controls\BaseControl
+	 * ToOneContainerFactory constructor.
+	 * @param $name
+	 * @param $containerFactory
+	 * @param null $entityFactory
+	 * @param null $isFilledComponentName
 	 */
-	private ?Nette\Forms\Controls\BaseControl $isFilledComponent = null;
-
-	/**
-	 * ToOneContainer constructor.
-	 * @param string $entityFieldName
-	 * @param Closure $containerFactory
-	 * @param Closure|null $entityFactory
-	 * @param string|null $isFilledComponentName
-	 */
-	public function __construct(string $entityFieldName, Closure $containerFactory, ?Closure $entityFactory, ?string $isFilledComponentName)
+	public function __construct($name, $containerFactory, $entityFactory = null, $isFilledComponentName = null)
 	{
+		$this->name = $name;
+		$this->containerFactory = $containerFactory;
 		$this->entityFactory = $entityFactory;
-
-		$this->monitor(Presenter::class, function() use ($entityFieldName, $containerFactory, $isFilledComponentName) {
-			$containerFactory->call($this->getForm(), $this);
-
-			if ($isFilledComponentName) {
-				$this->setIsFilledComponent($this->addText($isFilledComponentName)->setHtmlAttribute('style', 'display: none'));
-			}
-		});
+		$this->isFilledComponentName = $isFilledComponentName;
 	}
 
 	/**
-	 * @param $message
-	 * @throws Exception
+	 * @return ToOneContainer
 	 */
-	public function validate(?array $controls = NULL): void
+	public function create()
 	{
-		if (
-			$this->isRequired()
-			&&
-			$this->isEmpty()
-		) {
-			$this->addText(static::ERROR_CONTROL_NAME)
-				->addError($this->getRequiredMessage());
-		}
-	}
-
-	/**
-	 * @param Nette\Forms\Controls\BaseControl $isFilledComponent
-	 * @return $this
-	 */
-	public function setIsFilledComponent(Nette\Forms\Controls\BaseControl $isFilledComponent)
-	{
-		$this->isFilledComponent = $isFilledComponent;
-		return $this;
-	}
-
-	/**
-	 * @return Nette\Forms\Controls\BaseControl
-	 */
-	public function getIsFilledComponent()
-	{
-		return $this->isFilledComponent;
-	}
-
-	/**
-	 * @param bool $excludeIsFilledComponent
-	 * @return bool
-	 */
-	public function isEmpty($excludeIsFilledComponent = false)
-	{
-		$values = $this->getValues('array');
-		if ($excludeIsFilledComponent) {
-			unset($values[$this->getIsFilledComponent()->getName()]);
-		}
-		foreach ($values as &$_value) {
-			if ($_value instanceof Nette\Http\FileUpload && !$_value->isOk()) {
-				$_value = null;
-			}
-		}
-
-		return !array_filter($values);
-	}
-
-	/**
-	 * @param ClassMetadata $relationMeta
-	 * @return mixed|object
-	 */
-	public function createEntity(ClassMetadata $relationMeta)
-	{
-		if (! $this->entityFactory) {
-			return $relationMeta->newInstance();
-		}
-
-		return call_user_func($this->entityFactory);
+		return new ToOneContainer($this->name, $this->containerFactory, $this->entityFactory, $this->isFilledComponentName);
 	}
 }
